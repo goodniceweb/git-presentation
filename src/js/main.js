68d3fac5bdbb58de574fd126e4ca54b477f59fc8
Alexey Cherkashin <goodniceweb@gmail.com>
2016-11-21 16:20:37 +0300

    Customize

--- src/js/main.js
+++ src/js/main.js
@@ -6,7 +6,7 @@ var slideShow = remark.create({
   // Set the slideshow display ratio
   // Default: '4:3'
   // Alternatives: '16:9', ...
-  ratio: '4:3',
+  ratio: '16:9',
 
   // Navigation options
   navigation: {
@@ -27,7 +27,7 @@ var slideShow = remark.create({
   },
 
   // Customize slide number label, either using a format string..
-  slideNumberFormat: 'Slide %current% of %total%',
+  // slideNumberFormat: 'Slide %current% of %total%',
   // .. or by using a format function
   slideNumberFormat: function (current, total) {
     return 'Slide ' + current + ' of ' + total;
